9a7fd4ab8c2c76b9dd26181f3f5d60adf2f3611b
stefanelly <stefanelly.julien@gmail.com>
2023-02-15 08:54:13 +0100

    CardType

--- src/Card.hpp
+++ src/Card.hpp
@@ -1,1 +1,27 @@
 #pragma once
+#include <iostream>
+#include <string>
+
+class Card
+{
+};
+
+enum class CardType
+{
+    Monster,
+    Spell,
+    Trap
+};
+
+std::string to_string(CardType cardType)
+{
+    switch (cardType)
+    {
+    case CardType::Monster:
+        return "Monster";
+    case CardType::Spell:
+        return "Spell";
+    case CardType::Trap:
+        return "Trap";
+    }
+}
