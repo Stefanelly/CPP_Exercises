cc36be29138d9bde3eb678ee3969eb27a9595ea0
stefanelly <stefanelly.julien@gmail.com>
2023-02-15 10:22:52 +0100

    spell trap

--- src/Spell.hpp
+++ src/Spell.hpp
@@ -1,5 +1,40 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include "Card.hpp"
 
-class Spell
+enum class SpellType
 {
+    Normal,
+    Equip,
+    Continuous,
+    QuickPlay,
+    Field,
+};
+
+const std::string to_string(SpellType type)
+{
+    switch (type)
+    {
+    case SpellType::Normal:
+        return "Normal";
+    case SpellType::Equip:
+        return "Equip";
+    case SpellType::Continuous:
+        return "Continuous";
+    case SpellType::QuickPlay:
+        return "Quick-Play";
+    case SpellType::Field:
+        return "Field";
+    }
+}
+
+class Spell : public Card
+{
+public:
+    Spell(const std::string &id, const std::string &name, const SpellType &type) : Card{id, CardType::Spell}, _type{type} { set_name(name); }
+    const SpellType &get_spell_type() { return _type; }
+
+private:
+    const SpellType &_type;
 };
--- src/Trap.hpp
+++ src/Trap.hpp
@@ -1,5 +1,35 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include "Card.hpp"
 
-class Trap
+enum class TrapType
 {
+    Normal,
+    Continuous,
+    Counter
+};
+
+std::string to_string(TrapType type)
+{
+    switch (type)
+    {
+    case TrapType::Normal:
+        return "Normal";
+    case TrapType::Continuous:
+        return "Continuous";
+    case TrapType::Counter:
+        return "Counter";
+    }
+}
+
+class Trap : public Card
+{
+public:
+    Trap(const std::string &id, const std::string &name, const TrapType &type) : Card{id, CardType::Trap}, _type{type} { set_name(name); }
+
+    const TrapType &get_trap_type() { return _type; }
+
+private:
+    const TrapType &_type;
 };
